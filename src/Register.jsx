59c26b28a69f358fe8f83e874a6b88275d118b98
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-15 01:46:43 +0300

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,13 +1,50 @@
 // flow
 import React from 'react';
 import {
-  Button
+  Row, Col,
+  Button,
+  Input
 } from 'reactstrap';
 
-class Register extends React.Component {
+type Group = {
+  unitId: String,
+  id: String,
+  symbol: String,
+  name: String,
+  opened: String
+}
+
+type State = {
+  group: Group
+}
+
+class Register extends React.Component<{}, State> {
+
+  state = {
+    group: {}
+  }
 
   constructor(props) {
     super(props);
+  }
+
+  async componentDidMount() {
+
+    const groupSymbol = this.props.match.params.groupSymbol;
+    const url = `https://us-central1-theta-1524876066401.cloudfunctions.net/api/group?symbol=${groupSymbol}`
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: "cors"
+    });
+
+    if( !response.ok ) {
+      throw Error(resp.statusText)
+    }
+
+    const _group = await response.json();
+    this.setState({
+      group: _group
+    });
   }
 
   register() {
@@ -34,7 +71,7 @@ class Register extends React.Component {
         throw Error(response.statusText)
       }
 
-      return resp.json();
+      return response.json();
     })
     .then( pupil => {
       console.log(pupil);
@@ -47,12 +84,67 @@ class Register extends React.Component {
   }
 
   render() {
-    return (<React.Fragment>
-      <div>
-        Register
+    return (
+      <div className="d-flex align-items-center flex-column justify-content-center h-100 text-white start-wrapper">
+        <h1 className="display-4">רישום ל{this.state.group.name}</h1>
+        <div className='box'>
+          <div className="form-group">
+            <Row>
+              <Col className='col-4 text-right'>
+                <div>שם פרטי</div>
+                <div>
+                  <Input />
+                </div>
+              </Col>
+              <Col className='col-4 text-right'>
+                <div>שם משפחה</div>
+                <div>
+                  <Input />
+                </div>
+              </Col>
+              <Col className='col-4 text-right'>
+                <div>ת.זהות</div>
+                <div>
+                  <Input />
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="form-group">
+            <div className='text-right'>כתובת</div>
+            <div className="w-200">
+              <Input />
+            </div>
+          </div>
+          <div className="form-group">
+            <Row>
+              <Col className='col-6 text-right'>
+                <div>תאריך לידה</div>
+                <div className="w-200">
+                  <Input />
+                </div>
+              </Col>
+              <Col className='col-6 text-right'>
+                <div>טלפון בבית</div>
+                <div className="w-200">
+                  <Input />
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="form-group">
+            <div className='text-right'>הערות</div>
+            <div className="w-200">
+              <Input />
+            </div>
+          </div>
+          <div className="form-group">
+            <Button className="w-200"
+              onClick={::this.register}>Register</Button>
+          </div>
+        </div>
+
       </div>
-      <Button onClick={::this.register}>Register</Button>
-      </React.Fragment>
     )
   }
 
